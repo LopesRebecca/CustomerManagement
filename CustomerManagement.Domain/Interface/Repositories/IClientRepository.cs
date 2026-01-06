965984bf8807596bc3fcb245cfbbc724d49bb281
LopesRebecca <marialopeslelis@alu.ufc.br>
2026-01-06 17:06:37 -0300

    feat: criando camada de repositorios para as consultas base da entidade cliente

---
+++ CustomerManagement.Domain/Interface/Repositories/IClientRepository.cs
@@ -0,0 +1,12 @@
+﻿using CustomerManagement.Domain.Entities;
+using CustomerManagement.Domain.ValueObjects;
+
+namespace CustomerManagement.Domain.Interface.Repositories
+{
+    public interface IClientRepository
+    {
+        Task CreateAsync(ClientEntity cliente);
+        Task<ClientEntity?> GetByIdAsync(int id);
+        Task<bool> ExistDocumentNumberAsync(DocumentNumber documento);
+    }
+}
---
+++ CustomerManagement.Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,43 @@
+﻿using CustomerManagement.Domain.Entities;
+using CustomerManagement.Domain.Interface.Repositories;
+using CustomerManagement.Domain.ValueObjects;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace CustomerManagement.Infrastructure.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly ISessionFactory _sessionFactory;
+
+        public ClientRepository(ISessionFactory sessionFactory)
+        {
+            _sessionFactory = sessionFactory;
+        }
+
+        public async Task CreateAsync(ClientEntity cliente)
+        {
+            using var session = _sessionFactory.OpenSession();
+            using var transaction = session.BeginTransaction();
+
+            await session.SaveAsync(cliente);
+            await transaction.CommitAsync();
+        }
+
+        public async Task<ClientEntity?> GetByIdAsync(int id)
+        {
+            using var session = _sessionFactory.OpenSession();
+            return await session.GetAsync<ClientEntity>(id);
+        }
+
+        public async Task<bool> ExistDocumentNumberAsync(DocumentNumber documento)
+        {
+            using var session = _sessionFactory.OpenSession();
+
+            return await session.Query<ClientEntity>()
+                .AnyAsync(c =>
+                    c.DocumentNumber.Valor == documento.Valor
+                );
+        }
+    }
+}
