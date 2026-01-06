a616d21629c013e1e80e8d8442ed70077c0229cd
LopesRebecca <marialopeslelis@alu.ufc.br>
2026-01-06 17:05:51 -0300

    feat: adiciona mapeamento NHibernate e fábrica de sessões

---
+++ CustomerManagement.Infrastructure/Persistence/Maps/ClientMap.cs
@@ -0,0 +1,30 @@
+﻿using CustomerManagement.Domain.Entities;
+using FluentNHibernate.Mapping;
+
+namespace CustomerManagement.Infrastructure.Persistence.Maps
+{
+    public class ClientMap : ClassMap<ClientEntity>
+    {
+        public ClientMap()
+        {
+            Table("clients");
+
+            Id(x => x.Id)
+                .GeneratedBy.Increment();
+
+            Map(x => x.FantasyName)
+                .Column("fantasy_name")
+                .Not.Nullable();
+
+
+            Map(x => x.DocumentNumber.Valor)
+                  .Column("document_valor")
+                  .Not.Nullable();
+
+
+            Map(x => x.Active)
+                .Column("active")
+                .Not.Nullable();
+        }
+    }
+}
---
+++ CustomerManagement.Infrastructure/Persistence/NHibernateSessionFactory.cs
@@ -0,0 +1,24 @@
+﻿using CustomerManagement.Infrastructure.Persistence.Maps;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace CustomerManagement.Infrastructure.Persistence
+{
+    public class NHibernateSessionFactory
+    {
+        public static ISessionFactory CriarSessionFactory(string connectionString)
+        {
+            return Fluently.Configure()
+                .Database(
+                    PostgreSQLConfiguration.Standard
+                        .ConnectionString(connectionString)
+                        .ShowSql()
+                )
+                .Mappings(m =>
+                    m.FluentMappings.AddFromAssemblyOf<ClientMap>()
+                )
+                .BuildSessionFactory();
+        }
+    }
+}
